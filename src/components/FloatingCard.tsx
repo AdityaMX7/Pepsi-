453be17a4825d914ec2f557716c438231de5e0d4
AdityaMX7 <addy.mx777@gmail.com>
2026-05-11 19:59:46 +0530

    Update FloatingCard.tsx

--- src/components/FloatingCard.tsx
+++ src/components/FloatingCard.tsx
@@ -31,8 +31,8 @@ export default function FloatingCard() {
      <motion.div
   onClick={handleClick}
   animate={{
-    x: [0, 40, 80, 30, 0],
-    y: [0, -60, -20, -40, 0],
+    x: [0, 15, 25, 10, 0]],
+    y: [0, -20, -10, -25, 0],
   }}
   whileHover={{ scale: 1.08 }}
   transition={{
