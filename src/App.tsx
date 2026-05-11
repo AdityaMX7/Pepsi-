8a583343cac5fd574cb393d061c430155b3c0e7e
AdityaMX7 <addy.mx777@gmail.com>
2026-05-11 16:54:25 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import FloatingCard from "./FloatingCard";
 import { motion, AnimatePresence } from 'motion/react';
 import { useState, useEffect } from 'react';
 import { X, Sparkles } from 'lucide-react';
